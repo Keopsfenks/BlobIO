01bc344276aba8776465d3948f98aae34dfc67b1
Semih Gürbüz <sgurbuzofficial@gmail.com>
2024-04-01 00:41:01 +0300

    Player eat blob add stats  variable.

--- Assets/Scripts/Baits/EatenEvent.cs
+++ Assets/Scripts/Baits/EatenEvent.cs
@@ -8,6 +8,11 @@ namespace Baits {
 	public class EatenEvent : MonoBehaviour {
 		public event UnityAction<EatenEvent> eventHandler;
 		public List<EatenEvent> baitListReference;
+		private Players.Stats _pStats;
+
+		private void Awake() {
+			_pStats = FindObjectOfType<Players.Stats>();
+		}
 
 		public void OnEatenEvent() {
 			eventHandler?.Invoke(this);
@@ -17,6 +22,7 @@ namespace Baits {
 			eventHandler -= OnEatenAction;
 			baitListReference.Remove(bait);
 			Destroy(this.gameObject);
+			_pStats.SetBlobCount(1);
 		}
 	}
 }
--- Assets/Scripts/Players/Stats.cs
+++ Assets/Scripts/Players/Stats.cs
@@ -1,9 +1,16 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Players {
 	public class Stats : MonoBehaviour {
 		[SerializeField] private int blobCount;
 
+		public int GetBlobCount() {
+			return blobCount;
+		}
 
+		public void SetBlobCount(int count) {
+			this.blobCount += count;
+		}
 	}
 }
